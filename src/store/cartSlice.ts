54d12a675ac356e8cfc1b2874cabee7f9cdcf5c0
Willitonlima <willitonwilli@gmail.com>
2026-06-03 23:12:26 -0300

    deploy

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { CartItem, CartState, Product } from '../types'
+import { CartState, Product } from '../types'
 
 const initialState: CartState = {
   items: [],
